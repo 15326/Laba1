ff12fabb46bf4072c992d3aba474f3ed7adacb57
15326 <nastyakublitskaya19@gmail.com>
2018-05-05 11:04:22 +0300

    Инициализирует новый экземпляр класса Gun значением по умалчанию

---
+++ Laba1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Laba1_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            gun ObjGun = new gun();
+            Console.WriteLine(ObjGun);
+            gun oobGun = new gun("M16",7.62,1500);
+            Console.WriteLine(oobGun);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba1_1/gun.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Laba1_1
+{
+    public class gun
+    {
+        private string name;
+        private double kol;
+        private int dal;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Gun значением по умалчанию
+        /// </summary> 
+
+        public gun()
+        {
+            this.name = "AK-47";
+            this.kol = 7.62;
+            this.dal = 1800;
+
+        }
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Tovar значением 
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="kol">Колибр</param>
+        /// <param name="dal">Дальность</param>
+        public gun(string Name, double Kol, int Dal)
+        {
+            Name = name;
+            Kol = kol;
+            Dal = dal;
+        }
+        /// <summary>
+        /// Вход объекта класса
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "\n Имя: " + name + "\n колибр: " + kol + "\n дальность  : " + dal;
+        }
+    }
+}
